2c8461fd15476981f9bb2acf6d2b774551ce58fe
VisionDEV1 <83010451+VisionDEV1@users.noreply.github.com>
2025-04-26 19:33:44 +0100

    Update config.lua

--- vd-treasurehunt/config.lua
+++ vd-treasurehunt/config.lua
@@ -1,12 +1,18 @@
 Config = {}
 
 -- General Settings
-Config.UseTreasureMapItem = true    -- true = use treasure_map item to start
+Config.UseTreasureMapItem = false    -- true = use treasure_map item to start
 Config.UseNPC = true                -- true = allow starting from NPC too
 Config.UseTarget = true             -- true = use ox_target/qb-target, false = 3D Text [E]
 
 Config.Cooldown = 600 -- seconds (10 min cooldown between hunts)
 Config.Stages = 3     -- How many clue stages before final treasure
+
+Config.HuntLocations = {
+    vector3(-1590.0, 5190.0, 4.0),
+    vector3(-1580.0, 5180.0, 4.0),
+    -- add more spots
+}
 
 -- Rewards
 Config.CashReward = {min = 1000, max = 2500} -- cash reward range
@@ -17,9 +23,9 @@ Config.ItemRewards = {
 }
 
 -- NPC Settings
-Config.NPCModel = "s_m_m_pirate_01"  -- pirate ped model
+Config.NPCModel = "s_m_y_dockwork_01"
 Config.NPCLocation = {
-    coords = vector3(-1604.664, 5257.087, 3.974),
+    coords = vector3(-1604.64, 5257.08, 2.08),
     heading = 200.0
 }
 
